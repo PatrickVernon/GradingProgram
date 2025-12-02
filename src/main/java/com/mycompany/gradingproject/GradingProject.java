2a9432a4d5a4db9c053f819bb9b63923d55f67fa
PatrickVernon <pvernon@uxbridgecollege.ac.uk>
2025-12-02 12:19:13 +0000

    First commit

---
+++ src/main/java/com/mycompany/gradingproject/GradingProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradingproject;
+
+/**
+ *
+ * @author pvernon
+ */
+public class GradingProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
